32ad3b482096bb0026464d70327d32415f744b27
RenZMin <42882641+RenZMin@users.noreply.github.com>
2021-09-25 01:10:23 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,17 +18,9 @@ function App() {
       <h2>Deployed with 4everland!</h2>
       <p>
         This project was bootstrapped with{" "}
-        <a href="https://facebook.github.io/create-react-app/">
+        <a href="http://getuvw.xyz/">
           Create React App
         </a>{" "}
-        and contains three directories, <code>/public</code> for static assets,{" "}
-        <code>/src</code> for components and content, and <code>/api</code>{" "}
-        which contains a serverless <a href="https://golang.org/">Go</a>{" "}
-        function. See{" "}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
       </p>
       <br />
       <h2>The date according to Go is:</h2>
